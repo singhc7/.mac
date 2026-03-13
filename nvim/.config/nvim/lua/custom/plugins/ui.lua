82e8c93fd6cad1641d59efafffe5060469b5b072
Chahat <chahat@disroot.org>
2026-03-12 20:07:44 -0500

    Modularize Neovim configuration structure
    Extracted options, keymaps, and autocmds into distinct
    modules. Separated plugin configurations into individual
    files within the custom plugins directory to significantly
    improve maintainability and code organization.

---
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -0,0 +1,44 @@
+return {
+  { -- You can easily change to a different colorscheme.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  -- Highlight todo, notes, etc in comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    ---@module 'todo-comments'
+    ---@type TodoOptions
+    ---@diagnostic disable-next-line: missing-fields
+    opts = { signs = false },
+  },
+
+  { -- Collection of various small independent plugins/modules
+    'nvim-mini/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function() return '%2l:%-2v' end
+    end,
+  },
+}
