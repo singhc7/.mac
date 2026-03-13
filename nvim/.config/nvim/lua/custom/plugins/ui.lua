990464d8635f8d3890c9fa2124bf235995758a78
Chahat <chahat@disroot.org>
2026-03-13 03:11:56 -0500

    Update UI theme and statusline
    Switch the colorscheme to gruvbox-dark-hard and replace mini.statusline with lightline.vim for a refined aesthetic and improved information density.

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -1,15 +1,26 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'wincent/base16-nvim',
+    lazy = false, -- load at start
+    priority = 1000, -- load first
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd [[colorscheme gruvbox-dark-hard]]
+      vim.o.background = 'dark'
+      vim.cmd [[hi Normal ctermbg=NONE]]
+      -- Less visible window separator
+      vim.api.nvim_set_hl(0, 'WinSeparator', { fg = 1250067 })
+      -- Make comments more prominent -- they are important.
+      local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
+      vim.api.nvim_set_hl(0, 'Comment', bools)
+      -- Make it clearly visible which argument we're at.
+      local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
+      vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', {
+        fg = marked.fg,
+        bg = marked.bg,
+        ctermfg = marked.ctermfg,
+        ctermbg = marked.ctermbg,
+        bold = true,
+      })
     end,
   },
 
@@ -24,6 +35,48 @@ return {
     opts = { signs = false },
   },
 
+  -- nice bar at the bottom
+  {
+    'itchyny/lightline.vim',
+    lazy = false, -- also load at start since it's UI
+    config = function()
+      -- no need to also show mode in cmd line when we have bar
+      vim.o.showmode = false
+      vim.g.lightline = {
+        active = {
+          left = {
+            { 'mode', 'paste' },
+            { 'readonly', 'filename', 'modified' },
+          },
+          right = {
+            { 'lineinfo' },
+            { 'percent' },
+            { 'fileencoding', 'filetype' },
+          },
+        },
+        component_function = {
+          filename = 'LightlineFilename',
+        },
+      }
+      function _G.LightlineFilenameInLua(opts)
+        if vim.fn.expand '%:t' == '' then
+          return '[No Name]'
+        else
+          return vim.fn.getreg '%'
+        end
+      end
+      -- https://github.com/itchyny/lightline.vim/issues/657
+      vim.api.nvim_exec(
+        [[
+				function! g:LightlineFilename()
+					return v:lua.LightlineFilenameInLua()
+				endfunction
+				]],
+        true
+      )
+    end,
+  },
+
   { -- Collection of various small independent plugins/modules
     'nvim-mini/mini.nvim',
     config = function()
@@ -32,13 +85,6 @@ return {
 
       -- Add/delete/replace surroundings
       require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function() return '%2l:%-2v' end
     end,
   },
 }
