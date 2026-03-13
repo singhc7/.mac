5db702d5536de68efc8a77c534759817d641bac9
Chahat <chahat@disroot.org>
2026-03-13 03:10:52 -0500

    Implement new autocommands for navigation and formatting
    Add logic to restore cursor position on file open and configure filetype-specific formatting for text and documentation files.

--- nvim/.config/nvim/lua/custom/autocmds.lua
+++ nvim/.config/nvim/lua/custom/autocmds.lua
@@ -9,3 +9,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+-- jump to last edit position on opening file
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = '*',
+  callback = function(ev)
+    if vim.fn.line "'\"" > 1 and vim.fn.line "'\"" <= vim.fn.line "$" then
+      -- except for in git commit messages
+      -- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
+      if not vim.fn.expand('%:p'):find('.git', 1, true) then
+        vim.cmd 'exe "normal! g\'\\""'
+      end
+    end
+  end,
+})
+
+-- shorter columns in text because it reads better that way
+local text = vim.api.nvim_create_augroup('text', { clear = true })
+for _, pat in ipairs { 'text', 'markdown', 'mail', 'gitcommit' } do
+  vim.api.nvim_create_autocmd('Filetype', {
+    pattern = pat,
+    group = text,
+    command = 'setlocal spell tw=72 colorcolumn=73',
+  })
+end
+--- tex has so much syntax that a little wider is ok
+vim.api.nvim_create_autocmd('Filetype', {
+  pattern = 'tex',
+  group = text,
+  command = 'setlocal spell tw=80 colorcolumn=81',
+})
