479f92111fc29ef4c3c578953f72e92f051303da
Chahat <chahat@disroot.org>
2026-03-27 00:01:13 -0500

    Set -o interactivecomments in zsh

--- zsh/.zsh/options.zsh
+++ zsh/.zsh/options.zsh
@@ -40,7 +40,7 @@ setopt hist_ignore_space
 
 # --- General ---
 setopt autocd           # If a command is a directory, cd into it
-# setopt interactivecomments
+setopt interactivecomments  # Allow comments in interactive shells
 # setopt magicequalsubst  # Completion for path-like arguments
 # setopt notify           # Notify of background job completion immediately
 # setopt prompt_subst     # Allow parameter expansion in prompt
