eff14b7ef9a285b6cc74b6461a66280f91103683
Chahat <chahat@disroot.org>
2026-03-30 12:35:31 -0500

    Remove pipx aliases from zsh

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -22,7 +22,6 @@ if [[ "$OSTYPE" == linux* ]]; then
     alias update-all='sudo dnf upgrade && \
                       flatpak update && \
                       snap refresh && \
-                      { command -v pipx >/dev/null && pipx upgrade-all || true } && \
                       { command -v rustup >/dev/null && rustup update || true } && \
                       { command -v cargo-install-update >/dev/null && cargo install-update -a || true } && \
                       { command -v npm >/dev/null && npm update -g || true }'
@@ -30,7 +29,6 @@ if [[ "$OSTYPE" == linux* ]]; then
     # Clean up system caches, unused packages, and package manager artifacts
     alias clean-system='sudo dnf autoremove && \
                         flatpak uninstall --unused && \
-                        { command -v pipx >/dev/null && pipx interpreter prune || true } && \
                         { command -v pip >/dev/null && pip cache purge || true } && \
                         { command -v npm >/dev/null && npm cache clean --force || true } && \
                         { command -v cargo >/dev/null && { command -v cargo-cache >/dev/null && cargo cache -a || true }; true }'
